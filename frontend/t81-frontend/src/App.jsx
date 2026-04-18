83b8a569774d49a5444a5a3abb079dc351c25e1a
Alok Bhadauria <bhadauriaalok123@gmail.com>
2026-04-18 10:37:20 +0530

    fix google authentication redirect handling

--- frontend/t81-frontend/src/App.jsx
+++ frontend/t81-frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
   return (
-    <GoogleOAuthProvider clientId="224125299681-9s3nh7vrdfu27t4vh00t26gn3vq0gg75.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="224125299681-oo0mc2c1fdvgl719e2t6t560sn7491pl.apps.googleusercontent.com">
       <ThemeProvider>
         <SoundProvider>
           <ToastProvider>
